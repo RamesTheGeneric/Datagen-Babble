a446665046b2d1503626a0b0f6d3e3926229af78
Rames The Generic <53163624+RamesTheGeneric@users.noreply.github.com>
2023-10-16 00:16:07 -0400

    Initial V5 Datagen importer

---
+++ V5/Blender/DatasetGeneratorV5.0.py
@@ -0,0 +1,231 @@
+import bpy
+import pathlib
+import csv
+import random
+import numpy as np
+
+mesh = 'BabbleCA_M'
+ARKIT_CSV = "Z:\BabbleDataGeneration\BabbleDataGeneration\Humans 4.2.1\MySlate_4_iPhone.csv"
+MESH = bpy.data.objects[mesh]
+
+
+babble_shape_index = ["cheekPuffLeft", "cheekPuffRight", "cheekSuckLeft", "cheekSuckRight", "jawOpen", "jawForward", "jawLeft", "jawRight", "noseSneerLeft", "noseSneerRight", "mouthFunnel", "mouthPucker", "mouthLeft", "mouthRight", 
+    "mouthRollUpper", "mouthRollLower", "mouthShrugUpper", "mouthShrugLower", "mouthClose", "mouthSmileLeft", 
+    "mouthSmileRight", "mouthFrownLeft", "mouthFrownRight", "mouthDimpleLeft", "mouthDimpleRight", "mouthUpperUpLeft", 
+    "mouthUpperUpRight", "mouthLowerDownLeft", "mouthLowerDownRight", "mouthPressLeft", "mouthPressRight", "mouthStretchLeft", 
+    "mouthStretchRight", "tongueOut", "tongueUp", "tongueDown", "tongueLeft", "tongueRight", "tongueRoll", "tongueBendDown", "tongueCurlUp", "tongueSquish", "tongueFlat", "tongueTwistLeft", "tongueTwistRight"]
+
+class BabbleDefines():
+    shape_defs = dict(              # 45 shapes
+        cheekPuffLeft = [],
+        cheekPuffRight = [],
+        cheekSuckLeft = [],
+        cheekSuckRight = [],
+        jawOpen = [],
+        jawForward = [],
+        jawLeft = [],
+        jawRight = [],
+        noseSneerLeft = [],
+        noseSneerRight = [],
+        mouthFunnel = [],
+        mouthPucker = [],
+        mouthLeft = [],
+        mouthRight = [],
+        mouthRollUpper = [],
+        mouthRollLower = [],
+        mouthShrugUpper = [],
+        mouthShrugLower = [],
+        mouthClose = [],
+        mouthSmileLeft = [],
+        mouthSmileRight = [],
+        mouthFrownLeft = [],
+        mouthFrownRight = [],
+        mouthDimpleLeft = [],
+        mouthDimpleRight = [],
+        mouthUpperUpLeft = 	[],
+        mouthUpperUpRight = [],
+        mouthLowerDownLeft = [],
+        mouthLowerDownRight = [],
+        mouthPressLeft = [],
+        mouthPressRight = [],
+        mouthStretchLeft = [],
+        mouthStretchRight = [],
+        tongueOut = [],
+        tongueUp = [],
+        tongueDown = [],
+        tongueLeft = [],
+        tongueRight = [],
+        tongueRoll = [],
+        tongueBendDown = [],
+        tongueCurlUp = [],
+        tongueSquish = [],
+        tongueFlat = [],
+        tongueTwistLeft = [],
+        tongueTwistRight = [],
+    )
+
+sd = BabbleDefines.shape_defs
+ARKIT_SHAPES = []
+def all_values_below_threshold(lst, threshold):
+    return all(value < threshold for value in lst)
+
+def recon_cheekpuff(akl):  # Takes a cheekpuff input and creates values for cheekPuffLeft/Right
+    rtd = random.randint(0,2)
+    cheekPuff = akl[0]
+    # Combined even
+    if rtd == 0:
+        L = cheekPuff
+        R = cheekPuff
+    # Left Bias
+    if rtd == 1: 
+        LR = random.uniform(0,1)
+        L = cheekPuff
+        if LR == 1: R = random.uniform(0, cheekPuff)    
+        else: R = 0     #   Left Solo
+    # Right Bias
+    if rtd == 2: 
+        LR = random.uniform(0,1)
+        R = cheekPuff
+        if LR == 1: L = random.uniform(0, cheekPuff)    
+        else: L = 0     #   Right Solo
+    return L, R
+
+def recon_cheeksuck(akl):   # Checks if all values are near zero and adds values for cheekSuckLeft/Right
+    #print(akl)
+    Left,Right = 0,0
+    if all_values_below_threshold(akl, 0.15):
+        Left = random.uniform(0,1)
+        Right = random.uniform(0,1)
+    return Left, Right
+
+def recon_tongue(akl):   # Generates tongue shapes
+    rtd = random.uniform(0,10)
+    tongue = akl[28]
+    tongueUp, tongueDown, tongueLeft, tongueRight, tongueRoll, tongueBendDown, tongueCurlUp, tongueSquish, tongueFlat, tongueTwistLeft, tongueTwistRight = 0,0,0,0,0,0,0,0,0,0,0
+    if tongue >= 0.4:
+        rtd = random.randint(0,2)
+        if rtd == 0: # Directional
+            if random.randint(0,1):  # Up or Down
+                tongueUp = random.uniform(0, tongue)
+            else: 
+                tongueDown = random.uniform(0, tongue)
+            if random.randint(0,1):  # Left or Right
+                tongueLeft = random.uniform(0, tongue)
+            else: 
+                tongueRight = random.uniform(0, tongue)
+        if rtd == 1: # Twisty
+            if random.randint(0,1):  # Twist Left or Right
+                tongueTwistLeft = random.uniform(0, tongue)
+            else: 
+                tongueTwistRight = random.uniform(0, tongue)
+        if rtd == 2: # Twisty
+            roll = random.randint(0,2)
+            if roll == 0:  # Twist Left or Right
+                tongueRoll = random.uniform(0, tongue)
+            if roll == 1: 
+                tongueFlat = random.uniform(0, tongue)
+            if roll == 2: 
+                tongueSquish = random.uniform(0, tongue)
+    if tongue <= 0.4:
+        rtd = random.randint(0,2)
+        if rtd == 0: # Directional
+            if random.randint(0,1):  # Up or DownS
+                tongueCurlUp = random.uniform(0, tongue)
+            else: 
+                tongueBendDown = random.uniform(0, tongue)
+    
+    return tongueUp, tongueDown, tongueLeft, tongueRight, tongueRoll, tongueBendDown, tongueCurlUp, tongueSquish, tongueFlat, tongueTwistLeft, tongueTwistRight
+
+def fill_babble_shapes(akl, sd, bsi):
+    L,R = recon_cheekpuff(akl)
+    sd[bsi[0]].append(L)
+    sd[bsi[1]].append(R)
+    L,R = recon_cheeksuck(akl)
+    sd[bsi[2]].append(L)
+    sd[bsi[3]].append(R)
+    sd[bsi[4]].append(akl[1])
+    sd[bsi[5]].append(akl[5])
+    sd[bsi[6]].append(akl[3])
+    sd[bsi[7]].append(akl[4])
+    sd[bsi[8]].append(akl[29])
+    sd[bsi[9]].append(akl[30])
+    sd[bsi[10]].append(akl[8])
+    sd[bsi[11]].append(akl[9])
+    sd[bsi[12]].append(akl[14])
+    sd[bsi[13]].append(akl[15])
+    sd[bsi[14]].append(akl[6])
+    sd[bsi[15]].append(akl[7])
+    sd[bsi[16]].append(akl[24])
+    sd[bsi[17]].append(akl[25])
+    sd[bsi[18]].append(akl[2]) # mouthClose
+    sd[bsi[19]].append(akl[16])
+    sd[bsi[20]].append(akl[17])
+    sd[bsi[21]].append(akl[18])
+    sd[bsi[22]].append(akl[19])
+    sd[bsi[23]].append(akl[22])
+    sd[bsi[24]].append(akl[23])
+    sd[bsi[25]].append(akl[10])
+    sd[bsi[26]].append(akl[11])
+    sd[bsi[27]].append(akl[12])
+    sd[bsi[28]].append(akl[13])
+    sd[bsi[29]].append(akl[26])
+    sd[bsi[30]].append(akl[27])
+    sd[bsi[31]].append(akl[20])
+    sd[bsi[32]].append(akl[21])
+    sd[bsi[33]].append(akl[28]) # Tongue Out
+    tongueUp, tongueDown, tongueLeft, tongueRight, tongueRoll, tongueBendDown, tongueCurlUp, tongueSquish, tongueFlat, tongueTwistLeft, tongueTwistRight = recon_tongue(akl)
+    sd[bsi[34]].append(tongueUp)
+    sd[bsi[35]].append(tongueDown)
+    sd[bsi[36]].append(tongueLeft)
+    sd[bsi[37]].append(tongueRight)
+    sd[bsi[38]].append(tongueRoll)
+    sd[bsi[39]].append(tongueBendDown)
+    sd[bsi[40]].append(tongueCurlUp)
+    sd[bsi[41]].append(tongueSquish)
+    sd[bsi[42]].append(tongueFlat)
+    sd[bsi[43]].append(tongueTwistLeft)
+    sd[bsi[44]].append(tongueTwistRight)
+
+with open(ARKIT_CSV, newline='') as csvfile:
+    arkit_list = []
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        shape_row = [float(row["CheekPuff"]), float(row["JawOpen"]), float(row["MouthClose"]), float(row["JawLeft"]), float(row["JawRight"]), float(row["JawForward"]), 
+        float(row["MouthRollUpper"]), float(row["MouthRollLower"]), float(row["MouthFunnel"]), float(row["MouthPucker"]), float(row["MouthUpperUpLeft"]), 
+        float(row["MouthUpperUpRight"]), float(row["MouthLowerDownLeft"]), float(row["MouthLowerDownRight"]), float(row["MouthLeft"]), float(row["MouthRight"]), 
+        float(row["MouthSmileLeft"]), float(row["MouthSmileRight"]), float(row["MouthFrownLeft"]), float(row["MouthFrownRight"]), float(row["MouthStretchLeft"]), float(row["MouthStretchRight"]), 
+        float(row["MouthDimpleLeft"]), float(row["MouthDimpleRight"]), float(row["MouthShrugUpper"]), float(row["MouthShrugLower"]), float(row["MouthPressLeft"]), float(row["MouthPressRight"]), float(row["TongueOut"]), float(row["NoseSneerLeft"]), float(row["NoseSneerRight"])]
+        arkit_list.append(shape_row)
+    for akl in enumerate(arkit_list):
+        fill_babble_shapes(akl[1], sd, babble_shape_index)
+
+def add_blendshape_fcurve(ob, shape_name, keyframes):
+    
+    shape_key = ob.data.shape_keys.key_blocks.get(shape_name)
+    if shape_key:
+        me = ob.data
+        me.shape_keys.animation_data_create()
+        action = bpy.data.actions["Key.004Action"]
+        fc = action.fcurves.new(f'key_blocks["{shape_key.name}"].value')
+        fc.keyframe_points.add(count=len(keyframes) // 2)
+        fc.keyframe_points.foreach_set("co", keyframes)
+
+start = 0
+end = 10000
+
+for shape_name in babble_shape_index:
+    print(shape_name)
+    keyframes = []
+    for kf in range(start, end): # List of random values representing generated ones
+        keyframes.append(kf)
+        keyframes.append(sd[shape_name][kf])  
+    for kf in range(start, end): # Random Calib offsets
+        min_value = 0
+        max_value = random.uniform(0.5, 1)
+        value = np.clip((sd[shape_name][kf] - min_value) / (max_value - min_value), 0, 1)
+        keyframes.append(kf + end)
+        keyframes.append(value)  
+    
+
+    add_blendshape_fcurve(bpy.context.object, shape_name, keyframes)
+MESH.data.shape_keys.animation_data.action
